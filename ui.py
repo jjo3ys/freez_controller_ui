0feae3f3bee3d83bc89911423e44ebdeb7ad10b1
jjo3ys <jjo3ys@naver.com>
2022-03-31 10:35:04 +0900

    <feat> update text with read csv file

--- ui.py
+++ ui.py
@@ -5,8 +5,9 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 from PIL import ImageTk, Image
+from matplotlib.pyplot import text
 
-w = 1000
+w = 900
 h = 800
 
 lupx = lupy = 20
@@ -20,11 +21,11 @@ rdpy = h-20
 root = Tk()
 root.title("Energy ui")
 root.resizable(False, False)
-root.geometry('1100x1000')
+root.geometry('1000x950')
 
 
 canvas = Canvas(root, width=w, height=h, bg="white", highlightbackground='black')
-canvas.place(x=50, y=100)
+canvas.place(x=50, y=75)
 
 # img = Image.open('refrigerator.png')
 # img = img.resize((300,150))
@@ -40,8 +41,8 @@ supply_pipe = [190, h/2+20,
                740, h-200,
                780, h-200,
                780, h-60,
-               230, h-60,#740
-               230, h/2+60,#460
+               230, h-60,
+               230, h/2+60,
                190, h/2+60]
 
 return_pipe = [190, h/2-20, 
@@ -55,14 +56,53 @@ return_pipe = [190, h/2-20,
                230, h/2-60,
                190, h/2-60]
 
-canvas.create_rectangle(lupx+20, h/2-60, lupx+170, h/2+60, fill='#0099ff')
-canvas.create_text(lupx+95, h/2, text="냉동기", font=('', 20))
+canvas.create_rectangle(30, 340, 190, 460, fill='#0099ff')
+canvas.create_text(115, 400, text="냉동기", font=('', 20))
+
+canvas.create_text(110, 260, text='터보식', font=('', 10))
+turbor1 = canvas.create_rectangle(35, 290, 65, 320, fill='#AEAEAE')
+canvas.create_text(50, 305, text='1', font=('', 10))
+turbor2 = canvas.create_rectangle(75, 290, 105, 320, fill='#AEAEAE')
+canvas.create_text(90, 305, text='2', font=('', 10))
+turbor3 = canvas.create_rectangle(115, 290, 145, 320, fill='#AEAEAE')
+canvas.create_text(130, 305, text='3', font=('', 10))
+turbor4 = canvas.create_rectangle(155, 290, 185, 320, fill='#AEAEAE')
+canvas.create_text(170, 305, text='4', font=('', 10))
+
+canvas.create_rectangle(45, 320, 55, 340, fill='#AEAEAE')
+canvas.create_rectangle(85, 320, 95, 340, fill='#AEAEAE')
+canvas.create_rectangle(125, 320, 135, 340, fill='#AEAEAE')
+canvas.create_rectangle(165, 320, 175, 340, fill='#AEAEAE')
+
+canvas.create_text(110, 540, text='흡수식', font=('', 10))
+absortion1 = canvas.create_rectangle(35, 510, 65, 480, fill='gray')
+canvas.create_text(50, 495, text='1', font=('', 10))
+absortion2 = canvas.create_rectangle(75, 510, 105, 480, fill='gray')
+canvas.create_text(90, 495, text='2', font=('', 10))
+absortion3 = canvas.create_rectangle(115, 510, 145, 480, fill='gray')
+canvas.create_text(130, 495, text='3', font=('', 10))
+absortion4 = canvas.create_rectangle(155, 510, 185, 480, fill='gray')
+canvas.create_text(170, 495, text='4', font=('', 10))
+
+canvas.create_rectangle(45, 480, 55, 460, fill='gray')
+canvas.create_rectangle(85, 480, 95, 460, fill='gray')
+canvas.create_rectangle(125, 480, 135, 460, fill='gray')
+canvas.create_rectangle(165, 480, 175, 460, fill='gray')
 
 canvas.create_rectangle(640, 200, 880, h-200, fill='gray')
 canvas.create_text(760, h/2, text='건물', font=('', 20))
 
 canvas.create_polygon(supply_pipe, fill='white', outline='black')
 canvas.create_polygon(return_pipe, fill='white', outline='black')
+
+canvas.create_text(500, 30, text='환수 파이프', font=("", 20))
+canvas.create_text(500, 670, text='공급 파이프', font=("", 20))
+
+canvas.create_text(85, 30, text='날짜/시간 :', font=('', 10), anchor='e')
+canvas.create_text(85, 50, text='외기 온도 :', font=('', 10), anchor='e')
+canvas.create_text(85, 70, text='공급 온도 :', font=('', 10), anchor='e')
+canvas.create_text(85, 90, text='환수 온도 :', font=('', 10), anchor='e')
+canvas.create_text(5, 110, text='건물 냉방 부하 :', font=('', 10), anchor='w')
 
 def select_file():
     filetypes = (
@@ -79,6 +119,13 @@ def select_file():
         try:
             lines = f.readlines()
             for line in lines:
+                color = 'orange'
+                date_time = canvas.create_text(90, 30, text='2022/03/01 17:00', font=('', 10), anchor='w')
+                degree = canvas.create_text(90, 50, text='33.1도', font=('', 10), anchor='w')
+                supply_degree = canvas.create_text(90, 70, text='7.1도', font=('', 10), anchor='w')
+                return_degree = canvas.create_text(90, 90, text='12.3도', font=('', 10), anchor='w')
+                rt = canvas.create_text(105, 110, text='33000RT', font=('', 10), anchor='w')
+
                 supply_coord = [190, 420, 190, 460]
                 return_coord = [190, 380, 190, 340]
                 supply_1 = canvas.create_polygon(supply_coord, fill='#0099ff')
@@ -111,28 +158,28 @@ def select_file():
 
 
                 time.sleep(0.5)
-                return_1 = canvas.create_polygon(return_coord, fill='orange')
+                return_1 = canvas.create_polygon(return_coord, fill=color)
                 for i in range(33):
                     sub_return_coord = [740, 380, 780, 380, 780, 380-i*10, 740, 380-i*10]
                     canvas.coords(return_1, sub_return_coord)
                     root.update()
                     time.sleep(0.01)
 
-                return_2 = canvas.create_polygon([740, 100, 740, 60], fill='orange')
+                return_2 = canvas.create_polygon([740, 100, 740, 60], fill=color)
                 for i in range(52):
                     sub_return_coord = [740, 60, 740, 100, 740-i*10, 100, 740-i*10, 60]
                     canvas.coords(return_2, sub_return_coord)
                     root.update()
                     time.sleep(0.01)
 
-                return_3 = canvas.create_polygon([230, 100, 270, 100], fill='orange')
+                return_3 = canvas.create_polygon([230, 100, 270, 100], fill=color)
                 for i in range(29):
                     sub_return_coord = [230, 100, 270, 100, 270, 100+i*10, 230, 100+i*10]
                     canvas.coords(return_3, sub_return_coord)
                     root.update()
                     time.sleep(0.01)
                 
-                return_4 = canvas.create_polygon([230, 340, 230, 380], fill='orange')
+                return_4 = canvas.create_polygon([230, 340, 230, 380], fill=color)
                 for i in range(9):
                     sub_return_coord = [230, 340, 230, 380, 230-i*10, 380, 230-i*10, 340]
                     canvas.coords(return_4, sub_return_coord)
@@ -140,13 +187,13 @@ def select_file():
                     time.sleep(0.01)
                     
                 time.sleep(0.5)
-                canvas.delete(supply_1, supply_2, supply_3, supply_4, return_1, return_2, return_3, return_4)
+                canvas.delete(supply_1, supply_2, supply_3, supply_4, return_1, return_2, return_3, return_4, date_time, degree, supply_degree, return_degree, rt)
                 
         except:
             messagebox.showwarning("파일 불러오기 오류", "파일을 불러올 수 없습니다.")
 
 open_button = Button(root, text='파일 열기', command = select_file)
-open_button.place(x=10, y=10)
+open_button.place(x=50, y=25)
 
 # arc1 = canvas.create_arc(lupx, lupy, lupx-50, lupy+50, start = 160, extent=120, style=ARC) # extent 호의 각도, style=ARC 일 때 호만, 없을 때 피자형태
 # point = [w/2-25, h/2+10, w/2+25, h/2+10, w/2, h/2-40]
